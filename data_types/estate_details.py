b51cbf3ae9d79500ce86d8ecd20703822aa8f069
tom <t-gutowski@o2.pl>
2023-03-12 11:35:34 +0100

    feat: add text filtering in validation of description

--- data_types/estate_details.py
+++ data_types/estate_details.py
@@ -1,11 +1,13 @@
-from pydantic import BaseModel, validator
 import unicodedata
+
+from pydantic import BaseModel, validator
 
 
 class EstateDetails(BaseModel):
-    """ 
+    """
     Estate details model class.
     """
+
     price: str
     size: str
     location: str
@@ -18,10 +20,17 @@ class EstateDetails(BaseModel):
     @validator("size", pre=True)
     def validate_size(cls, size: str) -> str:
         return size.replace(" m²", "")
-    
+
     @validator("description", pre=True)
     def validate_description(cls, description: str) -> str:
-        des = description.replace("Oferta wysłana z programu dla biur nieruchomości ASARI CRM ()", "").replace("\n", " ")
+        des: str = (
+            description.replace(
+                "Oferta wysłana z programu dla biur nieruchomości ASARI CRM ()",
+                "",
+            )
+            .replace("Oferta pochodzi z serwisu obido", "")
+            .replace("\n", " ")
+        )
         # Remove \xa0
         return unicodedata.normalize("NFKD", des)
 
