fbcef34cd75e691afcbe770908b006a9627b4a2d
tom <t-gutowski@o2.pl>
2023-03-12 11:36:52 +0100

    feat: multiple page and file writing thru main.py now

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from typing import List
+
+from config.config_handler import ParametersHandler
+from data_types.estate import Estate
 from scraper.otodom_scraper import OtoDomScraper
 
 if __name__ == "__main__":
 
-    scraper = OtoDomScraper()
-    s = scraper.get_listing_page_soup(page_no=1)
-    d = scraper.parse_page(soup=s)
+    scraper: OtoDomScraper = OtoDomScraper()
+    results: List[Estate] = scraper.parse_site()
+
+    scraper.save_data(
+        temp_path=ParametersHandler().get_params()["results_file"],
+        to_write=results,
+    )
