e2283280d736ff912afde1680a6e9cb308b7b24b
tom <t-gutowski@o2.pl>
2023-03-12 11:34:19 +0100

    docs: add docstrings

--- config/config_handler.py
+++ config/config_handler.py
@@ -5,6 +5,9 @@ import yaml
 
 
 class ParametersHandler:
+    """
+    Helper function to extract params from yaml and return predefined params.
+    """
     def __init__(self) -> None:
         self.handler: dict[Any, Any]
         current_directory: str = path.dirname(path.realpath(__file__))
@@ -15,24 +18,33 @@ class ParametersHandler:
             self.handler = yaml.safe_load(f)
 
         if not self.handler:
-            raise ValueError("Parameters file is empty")
+            raise ValueError("Parameters file is empty or not exists.")
 
     def get_params(self) -> Dict[str, Any]:
+        """
+        Return all handler params from "parameters.yaml" in a Dict.
+        """
         return self.handler if self.handler else {}
 
 
 class PresetsHandler:
+    """
+    Helper function to extract params from yaml and return predefined params.
+    """
     def __init__(self) -> None:
         self.handler = None
-        current_directory = path.dirname(path.realpath(__file__))
-        file_path = path.normpath(
+        current_directory: str = path.dirname(path.realpath(__file__))
+        file_path: str = path.normpath(
             path.join(current_directory, "presets.yaml")
         )
         with open(file_path, "r", encoding="utf-8") as f:
             self.handler = yaml.safe_load(f)
 
         if not self.handler:
-            raise ValueError("Presets file is empty")
+            raise ValueError("Presets file is empty or not exists.")
 
-    def get_presets(self):
+    def get_presets(self) -> Dict[str, Any]:
+        """
+        Return all handler presets from "presets.yaml" in a Dict.
+        """
         return self.handler if self.handler else {}
